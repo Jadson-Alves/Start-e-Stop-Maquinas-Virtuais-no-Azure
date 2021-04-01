c81996fd3cecc85b52c46c68bc44d17af7258fd0
Jádson Alves <80630862+Jadson-Alves@users.noreply.github.com>
2021-03-31 21:30:09 -0300

    Add files via upload

---
+++ 14 - Start e Stop de VMs.ps1
@@ -0,0 +1,18 @@
+﻿Param(
+[string]$VmName,
+[string]$ResourceGroupName,
+[ValidateSet("Iniciar", "Desligar")]
+[string]$VmAction
+)
+# Autenticar na conta de automação
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID `
+-ApplicationID $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+# Iniciar VM
+IF ($VmAction -eq "Iniciar") {
+Start-AzureRmVM -Name $VmName -ResourceGroupName $ResourceGroupName
+}
+# Desligar VM
+IF ($VmAction -eq "Desligar") {
+Stop-AzureRmVM -Name $VmName -ResourceGroupName $ResourceGroupName -Force
+}
